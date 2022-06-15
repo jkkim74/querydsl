07bb7087124de1bf5c41c6ac0317e06ad2c12daa
Kim Jae Ki <jack68@naver.com>
2022-06-15 14:21:39 +0900

    domain creation

---
+++ src/main/java/com/example/querydsl/entity/Member.java
@@ -0,0 +1,38 @@
+package com.example.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Setter @Getter
+@ToString(of = {"id","username","age"})
+public class Member {
+
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+
+    }
+}
